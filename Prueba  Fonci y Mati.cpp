c779b380eb3eebc473bd34400326dc01d9458560
Mati9284 <matiaseest5@gmail.com>
2025-05-15 10:37:48 -0300

    Add files via upload

---
+++ Prueba  Fonci y Mati.cpp
@@ -0,0 +1,82 @@
+#include <iostream> // Solicitar biblioteca
+using namespace std;
+
+int main() // Funcion principal
+{
+	int edad; // Variable de la edad
+	int dia; // Variable del dia
+	float precio; // Variable del precio
+	float descuento1; // Variable del porcentaje
+	float precio_con_descuento; // Variable del precio con descuento
+	cout << "Este programa indica con el precio del producto el descuento dependiendo el dia y la edad" << endl; // Presentacion
+	cout << endl; // Separacion
+	cout << "Ingrese precio" << endl; // Pedirle al usuario que ingrese el precio
+	cin >> precio; // Datos del precio guardado en variable
+	cout << endl; // Separacion
+	cout << "Ahora porfavor ingrese su edad" << endl; // Pedirle al usuario que ingrese la edad
+	cin >> edad; // Datos de la edad guardado en variable
+	cout << endl; // Separacion
+	cout << "Ingrese el dia de la semana entre 1 que es lunes y 7 que es domingo" << endl; // Pedirle al usuario que ingrese el dia
+	cin >> dia; // Datos del dia guardado en variable
+	cout << endl; // Separacion
+	if(edad < 12) // Si la edad es menor a 12
+	{
+		if(dia == 2 or dia == 4) // Y si el dia es martes(2) o jueves(4)
+		{
+			descuento1 = precio * 0.20; // Calculo del descuento
+			precio_con_descuento = precio - descuento1; // Calculo del precio con descuento
+			cout << "El precio del producto es de: " << precio << " pesos" << endl; // Precio del producto base expresado en pantalla
+			cout << "El descuento que se aplica por la edad es de: " << "15%" << endl; // Precio del descuento de la edad expresado en pantalla
+			cout << "El descuento que se aplica por el dia es de: " << "5%" << endl; // Precio del descuento del dia expresado en pantalla
+			cout << "El precio final del producto es de: " << precio_con_descuento << " pesos" << endl; // Precio final del producto expresado en pantalla
+		}
+		else // Si el dia no es martes o jueves
+		{
+			descuento1 = precio * 0.15; // Calculo del descuento
+			precio_con_descuento = precio - descuento1; // Calculo del precio con descuento
+			cout << "El precio del producto es de: " << precio << " pesos" << endl; // Precio del producto base expresado en pantalla
+			cout << "El descuento que se aplica por la edad es de: " << "15%" << endl; // Descuento de la edad expresado en pantalla // Precio del descuento expresado en pantalla
+			cout << "El precio final del producto es de: " << precio_con_descuento << " pesos" << endl; // Precio final del producto expresado en pantalla
+		}
+	}
+	else
+	if(edad >= 60) // Si la edad es mayor o igual a 60
+	{
+		if(dia == 2 or dia == 4) // Y si el dia es martes o jueves
+		{
+			descuento1 = precio * 0.15; // Calculo del descuento
+			precio_con_descuento = precio - descuento1; // Calculo del precio con descuento
+			cout << "El precio del producto es de: " << precio << " pesos" << endl; // Precio del producto base expresado en pantalla
+			cout << "El descuento que se aplica por la edad es de: " << "10%" << endl; // Precio del descuento de la edad expresado en pantalla
+			cout << "El descuento por el dia es del: " << "5%" << endl; // Precio del descuento por el dia                         
+			cout << "El precio final del producto es de: " << precio_con_descuento << " pesos" << endl; // Precio final del producto expresado en pantalla
+		}
+		else // Si el dia no es martes o jueves
+		{
+			descuento1 = precio * 0.10; // Calculo del descuento
+			precio_con_descuento = precio - descuento1; // Calculo del precio con descuento
+			cout << "El precio del producto es de: " << precio << " pesos" << endl; // Precio del producto base expresado en pantalla
+			cout << "El descuento que se aplica por la edad es de: " << "10%" << endl; // Descuento de la edad expresado en pantalla
+			cout << "El precio final del producto es de: " << precio_con_descuento << " pesos"<< endl; // Precio final del producto expresado en pantalla
+		}
+	}
+	else
+	if(edad >= 12 and edad < 60) // Si la edad es mayor o igual a 12 y la edad es menor a 60
+	{
+		if(dia == 2 or dia == 4) // Y el dia es martes o jueves
+		{
+			descuento1 = precio * 0.05; // Calculo del descuento
+			precio_con_descuento = precio - descuento1; // Calculo del precio con descuento
+			cout << "El precio del producto es de: " << precio << " pesos" << endl; // Precio del producto base expresado en pantalla
+			cout << "El descuento que se aplica por el dia es de: " << "5%" << endl; // El descuento por el dia expresado en pantalla
+			cout << "El precio final del producto es de: " << precio_con_descuento << " pesos" << endl; // Precio final del producto expresado en pantalla
+		}
+		else // Si el dia no es ni martes o jueves
+		{
+			cout << "El precio del producto es de: " << precio << " pesos" << endl; // Precio del producto base expresado en pantalla
+			cout << "No se aplica ningun descuento" << endl; // Descuento expresado en pantalla
+			cout << "El precio final del producto es de: " << precio << " pesos" << endl; // Precio final del producto expresado en pantalla
+		}
+	}
+	return 0;
+}
